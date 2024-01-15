8bb5c7e24889c6492bbefd01b3d5c462a3d251d0
Rafael Estevam <rafael2003.estevam@gmail.com>
2024-01-15 15:08:50 +0000

    type1 e type2 feito

--- src/main/java/com/ips/tpsi/pokemonwebapp/entity/PokemonTypeLevel.java
+++ src/main/java/com/ips/tpsi/pokemonwebapp/entity/PokemonTypeLevel.java
@@ -16,22 +16,19 @@ import lombok.Setter;
 public class PokemonTypeLevel {
 
 
-
-
     @Id
+    @Column(name = "id_pokemon_type_level")
+    private Integer IdpokemonTypeLevel;
+
+
     @Column(name = "type_level")
     private Integer typeLevel;
-
-
 
 
     @ManyToOne
     @JoinColumn(name = "pokemon_character_id", referencedColumnName = "id_pokemon_character", foreignKey = @ForeignKey(name = "FK_pokemon_character"))
     private PokemonCharacter pokemonCharacterFK;
 
-
-
-    
 
     @ManyToOne
     @JoinColumn(name = "type_id", referencedColumnName = "id_type", foreignKey = @ForeignKey(name = "FK_pokemon_type"))
